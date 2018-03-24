3313b2bc863223dbcef763d2219a01f2e3a4557b
Gihwan Kwon <gihwankwon@Gihwanui-MacBook-Pro.local>
2018-03-24 15:51:03 -0700

    From MAC

--- chapter1/Q1.cpp
+++ chapter1/Q1.cpp
@@ -1,18 +1,42 @@
 // Is Unique:
 // Implement an algorithm to determine if a string has all unique characters. 
 // What if you cannot use additional data structures? 
+#include <string>
+#include <vector>
+#include <iostream> 
+using namespace std; 
 
+bool isUnique(string word); 
 
 int main()
 {
+  string first = "start";
+  string second = "second"; 
+  cout << isUnique(first) << endl; 
+  cout << isUnique(second) << endl;
   return 0; 
 }
 
 bool isUnique(string word)
-{
-  bool result; 
+{ 
+  if (word.size() > 128)
+  {
+    return false; 
+  }
+ 
+  bool list[128]; 
   
-
-
-  return result; 
+  for (int i = 0; i < word.length(); i++)
+  {
+    int temp = word[i]; 
+    if (list[temp])
+    {
+      return false;
+    }
+    else 
+    {
+      list[temp] = true;
+    }
+  }
+  return true;
 }
